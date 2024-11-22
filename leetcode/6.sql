14f8384efb834a1bea1333880b8d26483047fe20
shuamamine <sahasenjuti796@gmail.com>
2024-11-22 22:47:55 +0530

    joins

---
+++ leetcode/6.sql
@@ -0,0 +1,51 @@
+-- Table: Employees
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | id            | int     |
+-- | name          | varchar |
+-- +---------------+---------+
+
+-- Table: EmployeeUNI
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | id            | int     |
+-- | unique_id     | int     |
+-- +---------------+---------+
+
+-- Input: 
+-- Employees table:
+-- +----+----------+
+-- | id | name     |
+-- +----+----------+
+-- | 1  | Alice    |
+-- | 7  | Bob      |
+-- | 11 | Meir     |
+-- | 90 | Winston  |
+-- | 3  | Jonathan |
+-- +----+----------+
+
+-- EmployeeUNI table:
+-- +----+-----------+
+-- | id | unique_id |
+-- +----+-----------+
+-- | 3  | 1         |
+-- | 11 | 2         |
+-- | 90 | 3         |
+-- +----+-----------+  
+
+-- Output: 
+-- +-----------+----------+
+-- | unique_id | name     |
+-- +-----------+----------+
+-- | null      | Alice    |
+-- | null      | Bob      |
+-- | 2         | Meir     |
+-- | 3         | Winston  |
+-- | 1         | Jonathan |
+-- +-----------+----------+
+
+SELECT EMP.unique_id, E.name
+FROM Employees E
+LEFT JOIN EmployeeUNI EMP ON E.id = EMP.id;
